054024da251e6b91954282a63308e1cea2ca7a34
Zain786101 <zaynaliarain@gmail.com>
2024-04-24 02:57:22 +0500

    Event handler and hook States set on Form

--- src/Components/AddAccountForm.jsx
+++ src/Components/AddAccountForm.jsx
@@ -1,47 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addaccountform.css";
 
 function Form() {
+  const [formData, setFormData] = useState({
+    serial: "",
+    group: "",
+    upgradeStatus: "",
+    account: "",
+    proxy: "",
+    status: "",
+    action: "",
+  });
+  const handleInput = (event) => {
+    console.log(event.target.value);
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <div>
       <div className="user-form">
         <div className="container">
           <div className="row mt-5 justify-content-center">
             {/* <div className="col-6"></div> */}
-            <div className="col-6">
+            <div className="col-lg-6 col-sm-12 col-md-8">
               <form className="form-outer-group">
                 <div className="row mt-5">
-                  <div className="col-md-4 mb-3">
+                  <div className="col-md-4 col-sm-4 mb-3">
                     <label htmlFor="validationServer01">Serial No</label>
                     <input
                       type="text"
                       className="form-control"
                       id="validationServer01"
+                      name="serial"
+                      value={formData.serial}
+                      onChange={handleInput}
                       placeholder=""
-                      // value=""
                       required
                     />
                   </div>
-                  <div className="col-md-4 mb-3">
+                  <div className="col-md-4 col-sm-4 mb-3">
                     <label htmlFor="validationServer02">Group</label>
                     <input
                       type="text"
+                      name="group"
                       className="form-control"
                       id="validationServer02"
+                      value={formData.group}
+                      onChange={handleInput}
                       placeholder=""
-                      // value=""
                       required
                     />
                   </div>
-                  <div className="col-md-4 mb-3">
+                  <div className="col-md-4 col-sm-4 mb-3">
                     <label htmlFor="validationServerUsername">
                       Upgrade Status
                     </label>
                     <div className="input-group">
                       <input
                         type="text"
+                        name="upgradeStatus"
                         className="form-control"
                         id="validationServerUsername"
+                        value={formData.upgradeStatus}
+                        onChange={handleInput}
                         placeholder=""
                         aria-describedby="inputGroupPrepend3"
                         required
@@ -53,12 +74,15 @@ function Form() {
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-md-3 mb-3">
+                  <div className="col-md-3 col-sm-8 mb-3">
                     <label htmlFor="validationServer03">Account</label>
                     <input
                       type="text"
+                      name="account"
                       className="form-control"
                       id="validationServer03"
+                      value={formData.account}
+                      onChange={handleInput}
                       placeholder=""
                       required
                     />
@@ -66,12 +90,15 @@ function Form() {
                       Please provide a valid city.
                     </div> */}
                   </div>
-                  <div className="col-md-3 mb-3">
+                  <div className="col-md-3 col-sm-8 mb-3">
                     <label htmlFor="validationServer04">Proxy</label>
                     <input
                       type="text"
+                      name="proxy"
                       className="form-control"
                       id="validationServer04"
+                      value={formData.proxy}
+                      onChange={handleInput}
                       placeholder=""
                       required
                     />
@@ -79,25 +106,31 @@ function Form() {
                         Please provide a valid state.
                       </div> */}
                   </div>
-                  <div className="col-md-3 mb-3">
+                  <div className="col-md-3 col-sm-8 mb-3">
                     <label htmlFor="validationServer05">Status</label>
                     <input
                       type="text"
+                      name="status"
                       className="form-control"
                       id="validationServer05"
+                      value={formData.status}
+                      onChange={handleInput}
                       placeholder=""
                       required
                     />
                     {/* <div className="invalid-feedback">
                       Please provide a valid zip.
                     </div> */}
-                  </div>{" "}
-                  <div className="col-md-3 mb-3">
+                  </div>
+                  <div className="col-md-3 col-sm-8 mb-3">
                     <label htmlFor="validationServer05">Action</label>
                     <input
+                      name="action"
                       type="text"
                       className="form-control"
                       id="validationServer05"
+                      value={formData.action}
+                      onChange={handleInput}
                       placeholder=""
                       required
                     />
